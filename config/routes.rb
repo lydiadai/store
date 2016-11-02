7d9ac84c3c0bdd0ae874a8ec05cf19902dffa6ef
lydiadai <lyida0511@gmail.com>
2016-11-02 15:34:06 +0800

    admin CRud

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :products
+  root 'products#index'
+  namespace :admin do
+    resources :products
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
