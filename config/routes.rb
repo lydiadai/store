418c8bcce2fd6fe4322b74e6f43341596aa16f0c
lydiadai <lyida0511@gmail.com>
2016-11-03 10:33:24 +0800

    delete cart_item

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :products do
-    member do
-      post :add_to_cart
-    end
-  end
+    devise_for :users
 
-  namespace :admin do
-    resources :products
-  end
-  resources :carts do
-    post :current_cart
-  end
-  resources :cart_item
-  root 'products#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :products do
+        member do
+            post :add_to_cart
+        end
+    end
+
+    namespace :admin do
+        resources :products
+    end
+
+    resources :carts
+
+    resources :cart_items
+
+    root 'products#index'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
